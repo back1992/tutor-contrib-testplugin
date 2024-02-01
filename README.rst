510d06d52cdaff25cce5b64b964ebbcd10b5b3c3
back1992 <back1992@gmail.com>
2024-02-01 15:38:59 +0800

    initial

---
+++ README.rst
@@ -0,0 +1,22 @@
+testplugin plugin for `Tutor <https://docs.tutor.overhang.io>`__
+===================================================================================
+
+Installation
+------------
+
+::
+
+    pip install git+https://github.com/back1992/tutor-contrib-testplugin
+
+Usage
+-----
+
+::
+
+    tutor plugins enable testplugin
+
+
+License
+-------
+
+This software is licensed under the terms of the AGPLv3.
---
+++ setup.py
@@ -0,0 +1,61 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def load_readme():
+    with io.open(os.path.join(HERE, "README.rst"), "rt", encoding="utf8") as f:
+        return f.read()
+
+
+def load_about():
+    about = {}
+    with io.open(
+        os.path.join(HERE, "tutortestplugin", "__about__.py"),
+        "rt",
+        encoding="utf-8",
+    ) as f:
+        exec(f.read(), about)  # pylint: disable=exec-used
+    return about
+
+
+ABOUT = load_about()
+
+
+setup(
+    name="tutor-contrib-testplugin",
+    version=ABOUT["__version__"],
+    url="https://github.com/back1992/tutor-contrib-testplugin",
+    project_urls={
+        "Code": "https://github.com/back1992/tutor-contrib-testplugin",
+        "Issue tracker": "https://github.com/back1992/tutor-contrib-testplugin/issues",
+    },
+    license="AGPLv3",
+    author="back1992",
+    description="testplugin plugin for Tutor",
+    long_description=load_readme(),
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=["tutor"],
+    entry_points={
+        "tutor.plugin.v1": [
+            "testplugin = tutortestplugin.plugin"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
